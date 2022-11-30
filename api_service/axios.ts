1055d51047e2fb14dbbb3187e80b0d6a24e70064
rorn <rysarorn8@gmail.com>
2022-11-30 17:36:46 +0700

    go

--- api_service/axios.ts
+++ api_service/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 export const service = axios.create({
     withCredentials: true,
     // baseURL: "http://localhost:3001/api/v1",
-    baseURL: "https://sell-tool-api-nest-js.onrender.com/api/v1",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
-import { Provider } from "react-redux";
 import { wrapper } from "../redux/store";
 import { AnimatePresence } from "framer-motion";
-import { Router } from "next/router";
 
 function MyApp({ Component, pageProps , router}: AppProps) {
   return (
