8e0bc047b7d34857ee18a4a418af6a97ff1ceefb
rorn <rysarorn8@gmail.com>
2022-11-30 19:27:40 +0700

    changeENV

--- api_service/axios.ts
+++ api_service/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const service = axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:3001/api/v1",
-    // baseURL: process.env.NEXT_PUBLIC_API_URL,
+    // baseURL: "http://localhost:3001/api/v1",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
